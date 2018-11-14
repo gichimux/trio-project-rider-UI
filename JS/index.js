9daba6c69c6b11aae68fbee51ca373a99117101f
gichimu mwai <gichimumwai1@mail.com>
2018-11-14 14:25:16 +0300

    added job-confirm button

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+function jobDirection(){
+   document.getElementById("general-delivery-info").style.display="none";
+   document.getElementById("job-direction").style.display="block";
+}
+
+function totalInfo(){
+    document.getElementById("job-direction").style.display="none";
+    document.getElementByClass("container").innerHTML="total-info"
+
+}
