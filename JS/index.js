d5036f98cfd0e40dcdf2b964544d6f32680a064d
gichimu mwai <gichimumwai1@mail.com>
2018-11-19 14:48:15 +0300

    tried to integrate google maps

--- JS/index.js
+++ JS/index.js
@@ -8,3 +8,11 @@ function totalInfo(){
     document.getElementByClass("container").innerHTML="total-info"
 
 }
+
+function myMap() {
+  var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
